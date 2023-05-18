58238617ba9504341c5d62caccba3a600229868c
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 08:49:45 -0500

    Create ejercicio_4_3.py

---
+++ ejercicio_4_3.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+	# programa que  resuelvo ecuacion de primer grado de la forma ax + b = 0 
+	# version 1.2
+	# 2/03/2023
+	# programado por: Gabriel Gomez
+	# definicion de variables
+	a = float()
+	b = float()
+	x = float()
+	# inicializacion de lasvariables
+	a = 0.0
+	b = 0.0
+	x = 0.0
+	# captura  de datos
+	print("recordar que se trata de una ecuacion de primer grado de la forma  ax + b = 0")
+	print("cual es el valor de a")
+	a = float(input())
+	print("Cual es el valor de b ")
+	b = float(input()) 
+	# operaciones aritmeticas
+	x = (-b)/a # despeje de la ecuacion
+	print("el resultado de la ecuacion es: ",x)
